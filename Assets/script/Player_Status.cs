069cb3e3e5769217e0505ec581a3f236b3319937
yuito <izayoi226@gmail.com>
2024-04-27 14:07:03 +0900

    プレイヤーの動きを重力を伴うように変更＆天井のバーを作成

--- Assets/script/Player_Status.cs
+++ Assets/script/Player_Status.cs
@@ -5,28 +5,25 @@ using UnityEngine;
 public class Player_Status : MonoBehaviour
 {
     // Start is called before the first frame update
-    private float _speed = 3.0f;
+    private float forceMagnitude = 200.0f;
     private float _timer = 0.5f;
+    Rigidbody2D rb;
 
     void Start()
     {
-        
+       rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(_timer > 0) 
-        {
-            _timer -= Time.deltaTime;
-        }
+        if(_timer > 0) _timer -= Time.deltaTime;
 
-        if (Input.GetMouseButtonDown(0) && transform.parent != null)
+        if (transform.parent != null)
         {
-            Vector3 parentToChildCenter = transform.position - transform.parent.position;
-            transform.parent = null;
-            GetComponent<Rigidbody2D>().velocity = parentToChildCenter.normalized * _speed;
-            _timer = 0.5f;
+            rb.gravityScale = 0;
+
+            if (Input.GetMouseButtonDown(0)) MovePlayer();
         }
     }
 
@@ -36,5 +33,16 @@ public class Player_Status : MonoBehaviour
         {
             this.gameObject.transform.parent = collision.gameObject.transform;
         }
+    }
+
+    private void MovePlayer()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0.2f;
+        Vector3 parentToChildCenter = transform.position - transform.parent.position;
+        Vector2 targetDirection = parentToChildCenter.normalized;
+        transform.parent = null;
+        rb.AddForce(targetDirection * forceMagnitude);
+        _timer = 0.5f;
     }
 }
