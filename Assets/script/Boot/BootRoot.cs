45dcb28c1e18229f569bd1c610b0020f1192a68f
yuito <izayoi226@gmail.com>
2024-07-30 23:46:54 +0900

    fix BootScene

--- Assets/script/Boot/BootRoot.cs
+++ Assets/script/Boot/BootRoot.cs
@@ -5,9 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class BootRoot : MonoBehaviour
 {
+    [SerializeField] private GameObject eventSystem;
+
     // Start is called before the first frame update
     void Start()
     {
+        DontDestroyOnLoad(eventSystem);
         SceneManager.LoadScene("Title");
     }
 
