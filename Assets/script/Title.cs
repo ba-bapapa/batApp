856ac935ba826e0f552f42763daecc5e4d919e8e
yuito <izayoi226@gmail.com>
2024-07-30 23:47:25 +0900

    タイトルシーンの変更

--- Assets/script/Title.cs
+++ Assets/script/Title.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Title : MonoBehaviour
 {
+    [SerializeField] private Button startButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startButton.onClick.AddListener(LoadStart);
     }
 
     // Update is called once per frame
@@ -19,7 +22,7 @@ public class Title : MonoBehaviour
 
     public void LoadStart()
     {
-        SceneManager.LoadScene("MainScene");
+        SceneManager.LoadScene("Game");
     }
 
     public void LoadTitle()
